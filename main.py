8a482d058057b9acf9900d083182b7b1d2c75775
Tóth Ákos <78317734+tothakos0910@users.noreply.github.com>
2022-01-13 21:15:37 +0100

    question filter
    the same question could not not appearing once again

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 
 data = open("data.txt", "r")
 content_list = data.readlines()
+
 question = []
 answer = []
+voltmar = []
+
 pontok = 0
 osszesen = 0
 
@@ -17,10 +20,15 @@ for i in range(0,len(content_list)):
 
 while True:
     os.system('cls||clear')
+
     index = random.randint(0, len(content_list)-1)
+    while index in voltmar:
+        index = random.randint(0, len(content_list)-1)
+
     if osszesen >= 1:
         szazalek = (pontok/osszesen)*100
         print(f"Megválaszolt kérdések: {osszesen} - Helyes válaszok: {pontok}. ({szazalek}%)\n")
+
     print(question[index])
     player_answer = input("Válasz: ")
 
@@ -34,5 +42,8 @@ while True:
         print(f"Rossz megoldás!! ({answer[index]})")
         osszesen += 1
 
-
-    input("Nyomj Entert a folytatáshoz...")
+    voltmar.append(index)
+    if len(voltmar) == len(content_list):
+        exit()
+    else:
+        input("Nyomj Entert a folytatáshoz...")
