230d70848067785a6ea8412cd92ee5a3650eb8ef
Tóth Ákos <78317734+tothakos0910@users.noreply.github.com>
2022-01-13 17:58:20 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import random 
+import os
+
+data = open("data.txt", "r")
+content_list = data.readlines()
+question = []
+answer = []
+pontok = 0
+osszesen = 0
+
+data.close()
+
+for i in range(0,len(content_list)):
+    split = content_list[i].split('#')
+    question.append(split[0])
+    answer.append(split[1].strip())
+
+while True:
+    os.system('cls||clear')
+    index = random.randint(0, len(content_list)-1)
+    if osszesen >= 1:
+        szazalek = (pontok/osszesen)*100
+        print(f"Megválaszolt kérdések: {osszesen} - Helyes válaszok: {pontok}. ({szazalek}%)\n")
+    print(question[index])
+    player_answer = input("Válasz: ")
+
+    if player_answer.upper() == answer[index]:
+        print(f"Helyes megoldás!! ({answer[index]})")
+        pontok += 1
+        osszesen += 1
+    elif player_answer.upper() == 'E':
+        exit()
+    else:
+        print(f"Rossz megoldás!! ({answer[index]})")
+        osszesen += 1
+
+
+    input("Nyomj Entert a folytatáshoz...")
